5ffefe780680f549b80f1cb2eb2281342282e55b
ritishdas116@outlook.com <ritishdas116@outlook.com>
2024-01-25 21:43:19 +0530

    navigation

--- RDC1.js
+++ RDC1.js
@@ -4,7 +4,15 @@ export default function Rd(){
   <div className="head">
   <img src="logo.png"/>
   <span><h3>DAS STEEL</h3>
-  <i>Storage with elegance</i></span></div><hr/>
-  
+  <i>Storage with elegance</i></span></div>
+  <nav>
+  <ul className="navi">
+  <li><a href="">Home</a></li>
+  <li><a href="">Products</a></li>
+  <li><a href="">Cart</a></li>
+  <li><a href="">Orders</a></li>
+  <li><a href="">About</a></li>
+  </ul>
+  </nav>
   </div>);
 }
