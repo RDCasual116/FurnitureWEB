ff919f4a84196ede2ccd5ce13f815be143621aab
ritishdas116@outlook.com <ritishdas116@outlook.com>
2024-01-25 20:57:57 +0530

    HEADER

---
+++ RDC1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function Rd(){
+  return (<div>
+  <div className="head">
+  <img src="logo.png"/>
+  <span><h3>DAS STEEL</h3>
+  <i>Storage with elegance</i></span></div><hr/>
+  
+  </div>);
+}
